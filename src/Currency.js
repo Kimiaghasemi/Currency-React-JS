3e95ba82c68529783984e47bb08d4e0e096241c0
hooriehghahremani <71557918+hooriehghahremani@users.noreply.github.com>
2020-12-22 01:11:41 +0330

    Import Material UI & Final CSS

--- src/Currency.js
+++ src/Currency.js
@@ -33,49 +33,167 @@ function Currency() {
 export default Currency
 
 /*
-function App() {
-
-  return (
-    <div className="App">
-      
-     <main>
-       <div className="header">:نرخ ارزها</div>
-        <tbody>
-          <tr>
-          <tr><td className="green">دلار</td></tr> 
-          <tr><td className="red">یورو</td></tr>
-          <tr><td className="green">پوند انگلیس</td></tr>
-            <tr>
-                
-                <td className="red">درهم امارات</td>
-            </tr> 
-            <tr>
-                <td className="green">لیر ترکیه</td>
-            </tr> 
-          <tr><td className="red">یوان چین</td></tr> 
-          <tr><td className="green">ین ژاپن</td></tr> 
-          <tr><td className="red">دلار کانادا</td></tr> 
-          <tr><td className="green">دلار استرالیا</td></tr> 
-          <tr><td className="red">دلار نیوزلند</td></tr> 
-          <tr><td className="green">فرانک سوئیس</td></tr> 
-          <tr><td className="red">کرون سوئد</td></tr> 
-          <tr><td className="green">روبل روسیه</td></tr> 
-          <tr><td className="red">منات آذربایجان</td></tr> 
-          <tr><td className="green">درام ارمنستان</td></tr> 
-          <tr><td className="red">افغانی افغانستان</td></tr>
-         </tr>
-        </tbody>
-       
-        <tr>
-          <div className="footer">
-            <td >آخرین به روز رسانی </td>
-            <AutorenewIcon ></AutorenewIcon>
-          </div>
-        </tr>
-      </main>
-   </div>
-    
-  );
-
-}
+  <div className="header">:نرخ ارزها</div>
+       <div className="money">
+        <div className="arz" >
+          <div><img alt="US" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/US.svg"/></div>
+          <div className="type">دلار آمریکا</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+        </div>   
+        <div className="arz">
+        <div><img alt="Euro" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/1280px-Flag_of_Europe.svg.png"/></div>
+           <div className="type">یورو</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+           </div>
+            <div className="arz">
+            <div><img alt="AE" src="https://catamphetamine.gitlab.io/country-flag-icons/3x2/AE.svg"/></div>
+            <div className="type">درهم امارات</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+           <div><img alt="IN" src="https://catamphetamine.gitlab.io/country-flag-icons/3x2/IN.svg"/></div>
+            <div className="type">روپیه هند</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="TR" src="https://catamphetamine.gitlab.io/country-flag-icons/3x2/TR.svg"/></div>
+            <div className="type">لیر ترکیه</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="RY" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/RU.svg"/></div>
+            <div className="type">روبل روسیه</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="CN " src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/CN.svg"/></div>
+            <div className="type">یوان چین</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="KR" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/KR.svg"/></div>
+            <div className="type">وون کره جنوبی</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="CH" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/CH.svg"/></div>
+            <div className="type">فرانک سوئیس</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="JP" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/JP.svg"/></div>
+            <div className="type">ین ژاپن</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="CA" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/CA.svg"/></div>
+            <div className="type">دلار کانادا</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+           <div><img alt="GB" src="https://catamphetamine.gitlab.io/country-flag-icons/3x2/GB.svg"/></div>
+            <div className="type">پوند انگلیس</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="SE" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg"/></div>
+            <div className="type">کرون سوئد</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="NO" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/NO.svg"/></div>
+            <div className="type">کرون نروژ</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="IQ" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/IQ.svg"/></div>
+            <div className="type">دینار عراق</div>
+            <div>
+              <div className="cost">
+              <div>:قیمت</div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+            <div className="arz">
+            <div><img alt="AU" src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/AU.svg"/></div>
+            <div className="type">دلار استرالیا</div>
+            <div>
+              <div className="cost">
+              <div> :قیمت </div>
+              <div className="cost-s">{currency.sana.data[0].p</div>
+              </div>
+            </div>
+            </div>
+          
 */
--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,19 @@ import React from 'react'
 import LoopIcon from '@material-ui/icons/Loop'
 
 function Footer() {
+    
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
     return (
-        <div >
-            <LoopIcon />
+        <div className="footer">
+          <div><p>آخرین به روز رسانی : امروز 27 آذر</p></div>
+             <div >
+                <IconButton onClick={refreshPage}>
+                  <LoopIcon />
+                </IconButton>
+            </div>
         </div>
     )
 }
